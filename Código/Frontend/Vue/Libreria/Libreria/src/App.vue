b32e2540470f400db15c7d938238539f7a14d507
Sebastián de los Ángeles <luis.delos@docente.ceibal.edu.uy>
2026-04-12 13:11:21 -0300

    feat: agregar funcionalidad para añadir libros y mejorar la estructura del proyecto

--- Código/Frontend/Vue/Libreria/Libreria/src/App.vue
+++ Código/Frontend/Vue/Libreria/Libreria/src/App.vue
@@ -1,31 +1,198 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue';
+
+const verTelon = ref(false)
+
+const mostrarTelon = () => {
+  verTelon.value = !(verTelon.value)
+}
+
+</script>
 
 <template>
+  <div class="telon" v-if="verTelon">
+    <div class="nuevoLibro">
+      <div class="cerrar" @click="mostrarTelon()"></div>
+      <h2>Nuevo Libro</h2>
+      <div class="formulario">
+        <label for="isbn">ISBN:</label><input type="number" id="isbn">
+        <label for="titulo">Título:</label><input type="text" id="titulo">
+        <label for="autor">Autor:</label><input type="text" id="autor">
+        <label for="genero">Genero:</label><input type="text" id="genero">
+        <label for="editorial">Editorial:</label><input type="text" id="editorial">
+        <label for="anio_publicacion">Año de Publicación:</label><input type="number" placeholder="YYYY" min="1900"
+          max="2100" id="anio_publicacion">
+        <label for="precio_uyu">Precio ($U):</label><input type="number" id="precio_uyu" min="0" placeholder="$U">
+      </div>
+      <div class="boton btnAgregarLibro">Agregar Libro</div>
+    </div>
+  </div>
+
   <div class="base">
     <div class="encabezado">
       <h1>Listado de Libros</h1>
-      <div class="agregarLibro">
+      <div class="boton agregarLibro" @click="mostrarTelon()">
         +
       </div>
     </div>
-    <div class="listado">
+    <div class="contenido">
+      <div class="listado">
 
+      </div>
     </div>
+
   </div>
 </template>
 
 <style scoped>
-
-.base{
+.base {
+  position: absolute;
+  top: 0px;
   background-color: #f3e95f;
   width: 100%;
   height: 100%;
 }
 
-.encabezado{
+.encabezado {
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
   width: 100%;
   height: 15%;
   border-bottom: solid 1px black;
 }
 
+.telon {
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  background-color: rgba(50, 50, 50, 0.75);
+  z-index: 100;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.contenido {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 80%;
+}
+
+.agregarLibro {
+  height: 80%;
+  aspect-ratio: 1/1;
+  display: flex;
+  background-color: rgb(218, 153, 2);
+  border: 3px solid #49461c;
+  border-radius: 20px;
+  color: white;
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 3em;
+  align-items: center;
+  justify-content: center;
+}
+
+
+.listado {
+  height: 90%;
+  width: 80%;
+  background-color: #fdfbdf;
+  border-radius: 20px;
+  border: 3px #49461c solid;
+}
+
+.nuevoLibro {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 60%;
+  height: 60%;
+  background-color: #fdfbdf;
+  border-radius: 20px;
+  border: 3px rgb(218, 153, 2) solid;
+
+}
+
+.nuevoLibro h2 {
+  width: 100%;
+  text-align: center;
+}
+
+.nuevoLibro .formulario {
+  position: relative;
+  width: 90%;
+  left: 5%;
+  display: grid;
+  grid-template-columns: 35% 50%;
+  column-gap: 5%;
+  row-gap: 5px;
+  font-size: 1.5em;
+}
+
+.nuevoLibro .formulario:nth-child(2n+1) {
+  color: green;
+  font-weight: bolder;
+  text-align: left;
+  text-indent: 10px;
+}
+
+.nuevoLibro .btnAgregarLibro {
+  margin-top: 20px;
+  margin-bottom: 10px;
+  display: flex;
+  width: 35%;
+  padding-top: 5px;
+  padding-bottom: 5px;
+  background-color: rgb(218, 153, 2);
+  border: 3px solid #49461c;
+  border-radius: 20px;
+  color: white;
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 1.5em;
+  align-items: center;
+  justify-content: center;
+}
+
+.boton:hover {
+  -webkit-box-shadow: 5px 5px 7px -1px #49461c;
+  box-shadow: 5px 5px 7px -1px #49461c;
+  cursor: pointer;
+}
+
+.boton:active {
+  -webkit-box-shadow: 0px 0px 0px 0px #000000;
+  box-shadow: 0px 0px 0px 0px #000000;
+  cursor: pointer;
+}
+
+.cerrar {
+  position: absolute;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  left: auto;
+  right: 20px;
+  top: 20px;
+  width: 30px;
+  aspect-ratio: 1/1;
+  background-color: red;
+  color: white;
+  border: #690303 solid 2px;
+  border-radius: 25px;
+  font-weight: bolder;
+}
+
+.cerrar:hover {
+  -webkit-box-shadow: 3px 3px 4px -1px #49461c;
+  box-shadow: 3px 3px 4px -1px #49461c;
+  cursor: pointer;
+}
+
+.cerrar::after {
+  content: "x";
+}
 </style>
