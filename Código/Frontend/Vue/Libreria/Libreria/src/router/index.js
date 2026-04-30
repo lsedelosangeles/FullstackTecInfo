ae76d4ce6f5b1806512ec1d9671471549ab62f1b
Sebastián de los Ángeles <luis.delos@docente.ceibal.edu.uy>
2026-04-30 09:35:41 -0300

    Se añade elementos para sección de login en proyecto Libreria base

--- Código/Frontend/Vue/Libreria/Libreria/src/router/index.js
+++ Código/Frontend/Vue/Libreria/Libreria/src/router/index.js
@@ -1,8 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+import Inicio from '../../views/Inicio.vue'
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path:"/",
+      component:Inicio,
+      name:'inicio'
+    }
+  ],
 })
 
 export default router
---
+++ Código/Frontend/Vue/Libreria/Libreria/views/Inicio.vue
@@ -0,0 +1,64 @@
+<script setup>
+</script>
+
+<template>
+    <div class="base">
+        <div class="principal sombra_1 ">
+            <h1>Ingreso:</h1>
+            <label for="usuario">Usuario:</label>
+            <input type="text" id="usuario">
+            <label for="contrasenia">Contraseña:</label>
+            <input type="password" id="contrasenia">
+            <button>Iniciar Sesión</button>
+            <p></p>
+        </div>
+    </div>
+
+</template>
+
+<style scoped>
+.base {
+    display: flex;
+    width: 100vw;
+    height: 100vh;
+    background-color: #F2EBE3;
+    justify-content: center;
+    align-items: center;
+}
+
+.principal {
+    width: 25vw;
+    height: 50vh;
+    border-radius: 30px;
+    background-color: white;
+    display: grid;
+    grid-template-columns: repeat(1, 2fr, 1, 1fr);
+    grid-template-rows: repeat(2, 50px);
+    row-gap: 10px;
+    padding: 30px;
+}
+
+.principal h1 {
+    font-size: 30px;
+    grid-column: span 2;
+    text-align: center;
+}
+
+.principal label {
+    font-weight: bold;
+
+}
+
+.principal input {
+    height: 25px;
+}
+
+.principal button {
+    position: relative;
+    grid-column: span 2;
+    width: 75%;
+    left: 12.5%;
+    height: 40px;
+    border-radius: 10px;
+}
+</style>
